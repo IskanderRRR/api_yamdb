b8e9f58214c72879039299d507cc0168e1802575
Pavel Homov <pawel.khomov@yandex.ru>
2022-11-28 00:22:49 +0300

    Добавлены втÑTitlesViewSetÐ, CategoryViewSet, GenreViewSet, один фильтр и 4 сериализатора.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -46,3 +46,45 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitleListSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(read_only=True, many=True)
+    category = CategorySerializer(read_only=True)
+    rating = serializers.IntegerField()
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'genre',
+                  'category', 'description', 'rating')
+        model = Title
+
+
+class TitleCreateSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        many=True,
+        queryset=Genre.objects.all(),
+    )
+
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all(),
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Title
